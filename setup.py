07b73fbdc048f632025ee3006cc37e5402c54760
Jürgen Friedrich <contact@messfuchs.at>
2018-11-26 09:58:44 +0100

    working on rinex

--- setup.py
+++ setup.py
@@ -2,23 +2,32 @@
 
 # Learn more: https://github.com/kennethreitz/setup.py
 
-from setuptools import setup, find_packages
+import setuptools
 
 
 with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    license_text = f.read()
 
-setup(
-    name='rinex_parser',
-    version='0.1.0',
-    description='RinexParser',
+setuptools.setup(
+    name='RinexParser',
+    version='0.1.2.dev',
+    description='Parsing Rinex files (supports version 2+3)',
     long_description=readme,
-    author='Jürgen Fredriksson',
+    author='Juergen Fredriksson',
     author_email='juergen.fredriksson@bev.gv.at',
-    url='https://github.com/dach.pos/rinexparser',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    url='https://gitlab.com/dach.pos/rinexparser',
+    license=license_text,
+    # packages=['rinex_parser'],
+    packages=setuptools.find_packages(exclude=('tests', 'docs', 'env', '.vscode')),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        "Operating System :: POSIX :: Linux",
+    ]
 )
