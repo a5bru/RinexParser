9cb14207bfe84dd2d13b12d7a50f23c4900a07f8
Jürgen Fredriksson <jiargei@gmail.com>
2019-09-02 17:14:22 +0200

    still improving

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(os.path.join(here, 'LICENSE.txt'), encoding='utf-8') as f:
 
 setuptools.setup(
     name='RinexParser',
-    version='1.0.0',
+    version='1.0.1',
     description='Parsing Rinex files (supports version 2+3)',
     long_description=readme,
     long_description_content_type='text/markdown',
